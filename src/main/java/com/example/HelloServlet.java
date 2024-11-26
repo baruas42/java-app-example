779266e142383bf4658c69552ccb23061c65dd99
baruas42 <102461324+baruas42@users.noreply.github.com>
2024-11-26 21:46:11 +0545

    Update HelloServlet.java

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -17,10 +17,10 @@ public class HelloServlet extends HttpServlet {
                 + "h1 { color: #333; }"
                 + "p { font-size: 20px; color: #666; }"
                 + "</style></head><body>"
-                + "<h1>Hello, World!</h1>"
+                + "<h1>Saurab's sample java application!</h1>"
                 + "<p>Welcome to my stylish web application.</p>"
-		+ "<img src='https://i0.wp.com/media3.giphy.com/media/l3E6KC64glestQ7Sw/giphy.gif' alt='Silicon valley' /> <br> <br> "
-		+ "<img src='https://media1.tenor.com/m/_qj8lY8hT08AAAAC/keanu-reeves-blow-kiss.gif' alt='Keanu Reeves GIF' /> "
+		+ "<img src='https://media1.tenor.com/m/sczscPhkz4MAAAAd/waiting-bored.gif' alt='Borr' /> <br> <br> "
+		+ "<img src='https://media1.tenor.com/m/UO9ZhAR6J9sAAAAd/gif.gif' alt='DeadPool' /> "
                 + "</body></html>");
     }
 }
